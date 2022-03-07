2d5941f47eb8aec5a2ea61ad8ec11a07f7595d61
Ore Poran <oreporan@gmail.com>
2022-03-07 13:09:30 +0200

    done

--- index.js
+++ index.js
@@ -7,20 +7,27 @@ const run = async (job, settings, action, type) => {
       `[nexrender-action-upload-s3-presigned] action can be only run in postrender mode, you provided: ${type}.`
     );
   }
-  const {
-    input,
-    params: { url, contentType },
-  } = action;
-  const logger = settings;
-  let finalInput = input ?? job.output;
-  if (!path.isAbsolute(finalInput))
-    finalInput = path.join(job.workpath, finalInput);
-
-  logger.log(
-    `[nexrender-action-upload-s3-presigned] uploading to presigned-url: ${url}`
-  );
-
-  await uploadToS3(url, finalInput, contentType, logger);
+  try {
+    const {
+      input,
+      params: { url, contentType },
+    } = action;
+    const logger = settings;
+    let finalInput = input ?? job.output;
+    if (!path.isAbsolute(finalInput))
+      finalInput = path.join(job.workpath, finalInput);
+  
+    logger.log(
+      `[nexrender-action-upload-s3-presigned] uploading to presigned-url: ${url}`
+    );
+  
+    await uploadToS3(url, finalInput, contentType, logger);
+  } catch (error) {
+    logger.log(
+      `[nexrender-action-upload-s3-presigned] failed uploading to presigned-url: ${url} \n ${error?.message}`
+    );
+    throw error;
+  }
 };
 
 module.exports = run;
